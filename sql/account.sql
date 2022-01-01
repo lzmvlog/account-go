ce07f74ae1771f07938a800315d163c901b784fe
weijian.zhang <zhang1591313226@163.com>
2022-01-01 23:52:56 +0800

    update sql

--- sql/account.sql
+++ sql/account.sql
@@ -3,15 +3,15 @@
 
  Source Server         : 127.0.0.1
  Source Server Type    : MySQL
- Source Server Version : 80019
+ Source Server Version : 80027
  Source Host           : 127.0.0.1:3306
  Source Schema         : account
 
  Target Server Type    : MySQL
- Target Server Version : 80019
+ Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 26/12/2021 22:20:56
+ Date: 01/01/2022 23:50:09
 */
 
 SET NAMES utf8mb4;
@@ -22,28 +22,28 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `bills`;
 CREATE TABLE `bills`  (
-  `id` int(0) NOT NULL COMMENT 'id',
-  `sub_id` int(0) NULL DEFAULT NULL COMMENT '科目id',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `sub_id` int NULL DEFAULT NULL COMMENT '科目id',
   `sub_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '科目名称',
-  `direction` int(0) NULL DEFAULT NULL COMMENT '0 借 1 贷',
+  `direction` int NULL DEFAULT NULL COMMENT '0 借 1 贷',
   `amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '金额数量',
-  `create_date` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `create_date` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for subjects
 -- ----------------------------
 DROP TABLE IF EXISTS `subjects`;
 CREATE TABLE `subjects`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'id',
   `sub_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '科目名称',
-  `parent_id` int(0) NULL DEFAULT NULL COMMENT '父级id',
-  `direction` int(0) NULL DEFAULT NULL COMMENT '0 借 1 贷',
+  `parent_id` int NULL DEFAULT NULL COMMENT '父级id',
+  `direction` int NULL DEFAULT NULL COMMENT '0 借 1 贷',
   `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '编码',
-  `is_enable` int(0) NULL DEFAULT NULL COMMENT '0 启用 1 禁用',
+  `is_enable` int NULL DEFAULT NULL COMMENT '0 启用 1 禁用',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '会计科目表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '会计科目表' ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
